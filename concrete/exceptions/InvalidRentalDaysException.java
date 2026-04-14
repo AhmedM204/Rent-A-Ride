f42d1a46488616cc546a880357fb8bb17607b09f
Abdulrahman Nahshal <abooooody40@gmail.com>
2026-04-14 21:17:09 +0300

    chore: Add an exception for entering invalid rental days

---
+++ concrete/exceptions/InvalidRentalDaysException.java
@@ -0,0 +1,12 @@
+/*
+ * Name: Abdulrahman Nahshal
+ * ID: 452033303
+ */
+
+package concrete.exceptions;
+
+public class InvalidRentalDaysException extends RuntimeException {
+    public InvalidRentalDaysException(String message) {
+        super(message);
+    }
+}
